5ea1f6b9e5ef4636994ee2d75ffe4f05c1a71ed0
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2024-08-01 14:09:00 +0900

    コントラクトウォレットのコード

---
+++ contract/src/ContractWallet.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Consumer {
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function deposit() public payable {}
+}
+
+contract ContractWallet {
+    mapping(address => uint) public balance;
+    mapping(address => uint) public passwd_hash;
+    mapping(bytes => bool) private used_proof;
+
+    function _check_passwd(
+        address sent_from,
+        uint passwd_hash,
+        bytes memory proof
+    ) private {
+        // todo verify zkp proof.
+    }
+
+    function transfer(
+        address payable send_to,
+        uint amount,
+        uint passwd_hash,
+        bytes memory proof
+    ) public {
+        require(used_proof(proof), "This proof is already used.");
+        require(
+            _check_passwd(msg.sender, passwd_hash, proof),
+            "Invalid proof."
+        );
+        require(balance[msg.sender] >= amount, "Not enough money");
+
+        balance[msg.sender] -= amount;
+        send_to.transfer(amount);
+    }
+}
